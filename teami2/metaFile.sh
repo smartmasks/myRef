88f8885173b270c8c1314f0379d4501c4e774fe2
selvakumarm <smartmasks@users.noreply.github.com>
2019-10-01 10:52:26 +0530

    Create metaFile.sh

---
+++ teami2/metaFile.sh
@@ -0,0 +1,3 @@
+https://dribbble.com/shots/7193216-Cloud-Storage-Webapp-File-Viewing-UI
+https://dribbble.com/shots/7167814-Product-Concepts
+https://dribbble.com/shots/7114529-Job-Search-Board-Design/attachments/117268?mode=media
