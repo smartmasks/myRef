5c902742f823a42401c4336ac3ef8781da860b0c
selvakumarm <smartmasks@users.noreply.github.com>
2019-10-08 13:26:42 +0530

    Update metaFile.sh

--- teami2/metaFile.sh
+++ teami2/metaFile.sh
@@ -1,3 +1,7 @@
 https://dribbble.com/shots/7193216-Cloud-Storage-Webapp-File-Viewing-UI
 https://dribbble.com/shots/7167814-Product-Concepts
 https://dribbble.com/shots/7114529-Job-Search-Board-Design/attachments/117268?mode=media
+
+
+#mongoimport csv array
+ - https://stackoverflow.com/questions/44622394/import-csv-data-as-array-in-mongodb-using-mongoimport
